824acdc3e3bfa2f1d75d96fe7f746be1b4ed2212
mkao <mkaoibrahim@gmail.com>
2023-02-24 11:03:35 +0330

    Fixed som typos and Added Button Navigation

--- app/src/main/java/dev/mkao/jettynoteswithcompose/ui/theme/componets/Appdrawer.kt
+++ app/src/main/java/dev/mkao/jettynoteswithcompose/ui/theme/componets/Appdrawer.kt
@@ -1,13 +1,18 @@
 package dev.mkao.jettynoteswithcompose.ui.theme.componets
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,7 +24,7 @@ import androidx.compose.ui.unit.dp
 import dev.mkao.jettynoteswithcompose.ui.theme.JettyNotesWithComposeTheme
 
 @Composable
-private fun Appdrawer() {
+private fun AppDrawer() {
     Row(modifier = Modifier.fillMaxWidth()){
         Image(imageVector = Icons.Filled.Menu , contentDescription ="Drawer Header Icon Menu",
         colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
@@ -41,30 +46,65 @@ private  fun ScreenNavigationButton(
     val colors = MaterialTheme.colorScheme
 
     //for different states of btn if selected or unselected
-    val ImageAlpha = if (isSelected) {
+    val imageAlpha = if (isSelected) {
         2f
     } else {
      0.8f
     }
     //colors fpr the text for the states selected and unselected
-    val textcolor = if (isSelected) {
+    val textColor = if (isSelected) {
         colors.primary
     }
     else {
         colors.onSurface.copy(alpha = 0.8f)
     }
     //color for the states if button is un/selected
-    val backgroundcolor = if (isSelected){
+    val backgroundColor = if (isSelected){
         colors.primary.copy(alpha = 0.14f)
     }else {
         colors.surface
     }
+
+    Surface(//1) {
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp, top = 8.dp),
+        color = backgroundColor,
+        shape = MaterialTheme.shapes.small){
+        Row(horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clickable(onClick = onClick)
+            .fillMaxWidth()
+            .padding(6.dp)
+        ) {
+            Image(imageVector = icon,
+                contentDescription = "Navigation Button",
+            colorFilter = ColorFilter.tint(textColor),
+            alpha = imageAlpha)
+            Spacer(Modifier.width(18.dp))
+            Text(text = label,
+            style = MaterialTheme.typography.bodyMedium,
+            color = textColor,
+            modifier = Modifier.fillMaxWidth())
+        }
+   }
+
 }
-@Preview
+
+    @Preview
 @Composable
-fun AppdrawerPreview(){
+fun AppDrawerPreview(){
     JettyNotesWithComposeTheme {
-        Appdrawer()
+        AppDrawer()
         
+    }
+}
+@Composable
+@Preview
+fun ScreenNavigationButtonPreview(){
+    JettyNotesWithComposeTheme {
+        ScreenNavigationButton(icon = Icons.Filled.Home, label = "Jetty Notes" , isSelected = true,
+        onClick = { })
     }
 }
