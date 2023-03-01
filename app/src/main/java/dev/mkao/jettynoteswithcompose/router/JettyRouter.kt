5dd4e93019f33705eb145e40291bb55554a428f8
mkao <mkaoibrahim@gmail.com>
2023-03-02 03:14:33 +0330

    Added Sealed Class for Navigation Logic

--- app/src/main/java/dev/mkao/jettynoteswithcompose/router/JettyRouter.kt
+++ app/src/main/java/dev/mkao/jettynoteswithcompose/router/JettyRouter.kt
@@ -1,4 +1,25 @@
 package dev.mkao.jettynoteswithcompose.router
 
-class JettyRouter {
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+sealed class  Screen {
+    object  Notes : Screen()
+    object  DeleteNote : Screen()
+    object  Trash:Screen()
 }
+
+object JettyRouter  {
+/**
+ * Allows you to change the screen in the
+ * Also keeps track of the current screen.
+ *
+ **/
+  var currentScreen : Screen by MutableState(Screen.Notes)
+  fun navigateTo(destination: Screen){
+   /**The navigateTo function is a method of the JetNotesRouter object
+    * and it takes a destination parameter of type Screen
+    * which represents the screen that the user wants to navigate to
+    **/
+    currentScreen = Screen
+  }
+ }
