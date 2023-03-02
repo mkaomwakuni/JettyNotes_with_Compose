89d5a4f6ad4ce529ad10520f23de3bd155ea821b
mkao <mkaoibrahim@gmail.com>
2023-03-02 04:16:55 +0330

    Centering NoteColor

--- app/src/main/java/dev/mkao/jettynoteswithcompose/ui/theme/componets/NoteColor.kt
+++ app/src/main/java/dev/mkao/jettynoteswithcompose/ui/theme/componets/NoteColor.kt
@@ -12,10 +12,17 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun NoteColor(){
+fun NoteColor(modifier: Modifier = Modifier,
+              color: Color,
+              size: Dp,
+              padding:Dp = 0.dp,
+              border:Dp)
+{
     Box(modifier = Modifier
         .size(48.dp)
         .background(Color.Red)
@@ -33,5 +40,9 @@ fun NoteColor(){
 @Preview
 @Composable
 fun NoteColorPreview(){
-    NoteColor()
+    NoteColor(
+        color = Color.LightGray,
+        size = 40.dp,
+        border = 2.dp
+    )
 }
