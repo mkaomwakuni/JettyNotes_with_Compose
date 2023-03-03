ecb1ca6a090807fb88780f9444aeec5b14c83f4d
mkao <mkaoibrahim@gmail.com>
2023-03-03 22:30:30 +0330

    Fixed Colors /Fixed Minor Issues

--- app/src/main/java/dev/mkao/jettynoteswithcompose/ui/theme/Theme.kt
+++ app/src/main/java/dev/mkao/jettynoteswithcompose/ui/theme/Theme.kt
@@ -19,15 +19,17 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = rwGreen,
+    secondary = rwGreenDark,
+    tertiary = rwGreen
+
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = rwGreen,
+    secondary = rwGreenDark,
+    tertiary = rwGreen
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
--- app/src/main/java/dev/mkao/jettynoteswithcompose/ui/theme/componets/Note.kt
+++ app/src/main/java/dev/mkao/jettynoteswithcompose/ui/theme/componets/Note.kt
@@ -1,15 +1,12 @@
 package dev.mkao.jettynoteswithcompose.ui.theme.componets
 
 import android.annotation.SuppressLint
-import android.inputmethodservice.Keyboard
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
@@ -19,30 +16,61 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.mkao.jettynoteswithcompose.ui.theme.rwGreen
 
 
-    @SuppressLint("NotConstructor")
+@SuppressLint("NotConstructor")
     @Composable
     fun Note() {
         val backgroundShape: Shape = RoundedCornerShape( 4.dp)
-        Row( modifier = Modifier.fillMaxWidth()
-            .padding(8.dp)
-            .shadow(1.dp,backgroundShape)
-            .background(Color.White,backgroundShape)
-            .heightIn(min = 64.dp)){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .shadow(1.dp, backgroundShape)
+                .background(Color.White, backgroundShape)
+                .heightIn(min = 64.dp)
+          )
+        {
 
-         Box(modifier = Modifier
-             .size(40.dp)
-             .background(Color.Green)
-                 )
+            NoteColor(
+                //Applying some padding
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(start = 16.dp, end = 16.dp),
+                color = rwGreen,
+                size = 40.dp,
+                border = 1.dp )
+
             //Centering Text & Checkbox composable
          Column(modifier = Modifier
              .weight(1f)
-             .align(Alignment.CenterVertically) ){
-             Text(text = "Title", maxLines = 1)
-             Text(text = "Description", maxLines = 1)
+             .align(Alignment.CenterVertically)
+         ){
+             Text(
+                 text = "Title",
+                 color = Color.Black,
+                 maxLines = 1,
+                 style = TextStyle(
+                     fontWeight = FontWeight.Normal,
+                     fontSize = 16.sp,
+                     letterSpacing = 0.15.sp
+              )
+             )
+             Text(
+                 text = "Description",
+                 color = Color.Black.copy(alpha = 0.75f),
+                 maxLines = 1,
+                 style = TextStyle(
+                     fontWeight = FontWeight.Normal,
+                     fontSize = 14.sp,
+                     letterSpacing = 0.25.sp)
+             )
          }
              Checkbox(
                  checked = false,
