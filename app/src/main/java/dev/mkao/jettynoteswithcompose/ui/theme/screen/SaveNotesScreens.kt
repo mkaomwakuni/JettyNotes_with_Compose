7baf807e96d7c12073be8ac868965eaac539b7e3
mkao <mkaoibrahim@gmail.com>
2023-03-09 01:04:05 +0330

    Creating the ColorItem

--- app/src/main/java/dev/mkao/jettynoteswithcompose/ui/theme/screen/SaveNotesScreens.kt
+++ app/src/main/java/dev/mkao/jettynoteswithcompose/ui/theme/screen/SaveNotesScreens.kt
@@ -1,15 +1,18 @@
 package dev.mkao.jettynoteswithcompose.ui.theme.screen
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.colorspace.ColorModel
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -18,7 +21,7 @@ import kotlinx.coroutines.CoroutineStart
 
     @Composable
     fun ColorItem(
-        color: CoroutineStart,
+        color: ColorModel,
         onColorSelect: (ColorModel) -> Unit
     )
     {
@@ -44,4 +47,27 @@ import kotlinx.coroutines.CoroutineStart
 @Composable
 fun ColorItemPreview(){
     ColorItem(CoroutineStart.DEFAULT){}
+}
+@Composable
+private fun ColorPicker(
+    colors:List<ColorModel>,
+    onColorSelect: (ColorModel) -> Unit
+){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(text = "Picker",
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+        LazyColumn(modifier = Modifier.fillMaxWidth()){
+            items(colors.size){
+                itemIndex ->
+                val color = colors[itemIndex]
+                ColorItem(
+                    color = color,
+                    onColorSelect = onColorSelect )
+            }
+        }
+
+    }
 }
